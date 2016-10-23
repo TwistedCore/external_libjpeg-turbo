50e2488757a0e297bbb38536e9fe4beb4304754c
Alex Naidis <alex.naidis@linux.com>
2016-10-23 16:36:56 +0200

    libjpeg-turbo: Simplify and Commonize Makefile
    This commit moves all common definitions to the
    top and uses these as much as possible.
    This makes maintainibility of libjpeg-turbo easier
    and removes the unneeded duplicate definitions.
    
    Also at the end of the created common variables are unset.
    
    Change-Id: I14d8724a936b5e30a779c3b5a03a5a9a7be34371
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    # Conflicts:
    #	Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+
+# Set up common variables for usage across the libjpeg-turbo modules
 
 # By default, the build system generates ARM target binaries in thumb mode,
 # where each instruction is 16 bits wide.  Defining this variable as arm
@@ -7,13 +8,21 @@ include $(CLEAR_VARS)
 # is the same setting previously used by libjpeg.
 # TODO (msarett): Run performance tests to determine whether arm mode is still
 #                 preferred to thumb mode for libjpeg-turbo.
-LOCAL_ARM_MODE := arm
+libjpeg_turbo_common_arm_mode := arm
 
-LOCAL_CFLAGS := -O3 -fstrict-aliasing
-LOCAL_CFLAGS += -Wno-unused-parameter -Werror
-LOCAL_CFLAGS += -DJDCT_DEFAULT=JDCT_IFAST
+libjpeg_turbo_common_cflags := -O3 -fstrict-aliasing
+libjpeg_turbo_common_cflags += -Wno-unused-parameter
+libjpeg_turbo_common_cflags += -DJDCT_DEFAULT=JDCT_IFAST
 
-LOCAL_SRC_FILES := \
+# If we are certain that the ARM v7 device has NEON (and there is no need for
+# a runtime check), we can indicate that with a flag.
+ifeq ($(strip $(TARGET_ARCH)),arm)
+  ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    libjpeg_turbo_common_cflags += -D__ARM_HAVE_NEON__
+  endif
+endif
+
+libjpeg_turbo_common_src_files := \
     jcapimin.c jcapistd.c jccoefct.c jccolor.c jcdctmgr.c jchuff.c \
     jcinit.c jcmainct.c jcmarker.c jcmaster.c jcomapi.c jcparam.c \
     jcphuff.c jcprepct.c jcsample.c jctrans.c jdapimin.c jdapistd.c \
@@ -24,55 +33,64 @@ LOCAL_SRC_FILES := \
     jmemnobs.c jquant1.c jquant2.c jutils.c
 
 # ARM v7 NEON
-LOCAL_SRC_FILES_arm += simd/jsimd_arm_neon.S simd/jsimd_arm.c
-
-# If we are certain that the ARM v7 device has NEON (and there is no need for
-# a runtime check), we can indicate that with a flag.
-ifeq ($(strip $(TARGET_ARCH)),arm)
-  ifeq ($(ARCH_ARM_HAVE_NEON),true)
-    LOCAL_CFLAGS += -D__ARM_HAVE_NEON__
-  endif
-endif
+libjpeg_turbo_common_src_files_arm := simd/jsimd_arm_neon.S simd/jsimd_arm.c
 
 # ARM v8 64-bit NEON
-LOCAL_SRC_FILES_arm64 += simd/jsimd_arm64_neon.S simd/jsimd_arm64.c
+libjpeg_turbo_common_src_files_arm64 := simd/jsimd_arm64_neon.S simd/jsimd_arm64.c
 
 # x86 MMX and SSE2
-LOCAL_SRC_FILES_x86 += \
+libjpeg_turbo_common_src_files_x86 := \
       simd/jsimd_i386.c simd/jccolor-mmx.asm simd/jccolor-sse2.asm \
-      simd/jcgray-mmx.asm  simd/jcgray-sse2.asm simd/jcsample-mmx.asm \
-      simd/jcsample-sse2.asm simd/jdcolor-mmx.asm simd/jdcolor-sse2.asm \
-      simd/jdmerge-mmx.asm simd/jdmerge-sse2.asm simd/jdsample-mmx.asm \
-      simd/jdsample-sse2.asm simd/jfdctflt-3dn.asm simd/jfdctflt-sse.asm \
-      simd/jfdctfst-mmx.asm simd/jfdctfst-sse2.asm simd/jfdctint-mmx.asm \
-      simd/jfdctint-sse2.asm simd/jidctflt-3dn.asm simd/jidctflt-sse2.asm \
-      simd/jidctflt-sse.asm simd/jidctfst-mmx.asm simd/jidctfst-sse2.asm \
-      simd/jidctint-mmx.asm simd/jidctint-sse2.asm simd/jidctred-mmx.asm \
-      simd/jidctred-sse2.asm simd/jquant-3dn.asm simd/jquantf-sse2.asm \
-      simd/jquanti-sse2.asm simd/jquant-mmx.asm simd/jquant-sse.asm \
-      simd/jsimdcpu.asm
-LOCAL_ASFLAGS_x86 += -DPIC -DELF
+      simd/jcgray-mmx.asm  simd/jcgray-sse2.asm simd/jchuff-sse2.asm \
+      simd/jcsample-mmx.asm simd/jcsample-sse2.asm simd/jdcolor-mmx.asm \
+      simd/jdcolor-sse2.asm simd/jdmerge-mmx.asm simd/jdmerge-sse2.asm \
+      simd/jdsample-mmx.asm simd/jdsample-sse2.asm simd/jfdctflt-3dn.asm \
+      simd/jfdctflt-sse.asm simd/jfdctfst-mmx.asm simd/jfdctfst-sse2.asm \
+      simd/jfdctint-mmx.asm simd/jfdctint-sse2.asm simd/jidctflt-3dn.asm \
+      simd/jidctflt-sse2.asm simd/jidctflt-sse.asm simd/jidctfst-mmx.asm \
+      simd/jidctfst-sse2.asm simd/jidctint-mmx.asm simd/jidctint-sse2.asm \
+      simd/jidctred-mmx.asm simd/jidctred-sse2.asm simd/jquant-3dn.asm \
+      simd/jquantf-sse2.asm simd/jquanti-sse2.asm simd/jquant-mmx.asm \
+      simd/jquant-sse.asm simd/jsimdcpu.asm
+
+# x86-64 SSE2
+libjpeg_turbo_common_src_files_x86_64 := \
+      simd/jsimd_x86_64.c simd/jccolor-sse2-64.asm simd/jcgray-sse2-64.asm \
+      simd/jchuff-sse2-64.asm simd/jcsample-sse2-64.asm simd/jdcolor-sse2-64.asm \
+      simd/jdmerge-sse2-64.asm simd/jdsample-sse2-64.asm simd/jfdctflt-sse-64.asm \
+      simd/jfdctfst-sse2-64.asm simd/jfdctint-sse2-64.asm simd/jidctflt-sse2-64.asm \
+      simd/jidctfst-sse2-64.asm simd/jidctint-sse2-64.asm simd/jidctred-sse2-64.asm \
+      simd/jquantf-sse2-64.asm simd/jquanti-sse2-64.asm
+
+# MIPS and MIPS64
+libjpeg_turbo_common_src_files_mips := jsimd_none.c
+
+# Common ASFLAGS for x86/x86_64
+libjpeg_turbo_common_asflags := -DPIC -DELF
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := $(libjpeg_turbo_common_arm_mode)
+
+LOCAL_SRC_FILES := $(libjpeg_turbo_common_src_files)
+
+LOCAL_SRC_FILES_arm += $(libjpeg_turbo_common_src_files_arm)
+
+LOCAL_SRC_FILES_arm64 += $(libjpeg_turbo_common_src_files_arm64)
+
+LOCAL_SRC_FILES_x86 += $(libjpeg_turbo_common_src_files_x86)
+LOCAL_ASFLAGS_x86 += $(libjpeg_turbo_common_asflags)
 LOCAL_C_INCLUDES_x86 += $(LOCAL_PATH)/simd
 
 # x86-64 SSE2
-LOCAL_SRC_FILES_x86_64 += \
-      simd/jsimd_x86_64.c simd/jccolor-sse2-64.asm simd/jcgray-sse2-64.asm \
-      simd/jcsample-sse2-64.asm simd/jdcolor-sse2-64.asm \
-      simd/jdmerge-sse2-64.asm simd/jdsample-sse2-64.asm \
-      simd/jfdctflt-sse-64.asm simd/jfdctfst-sse2-64.asm \
-      simd/jfdctint-sse2-64.asm simd/jidctflt-sse2-64.asm \
-      simd/jidctfst-sse2-64.asm simd/jidctint-sse2-64.asm \
-      simd/jidctred-sse2-64.asm simd/jquantf-sse2-64.asm \
-      simd/jquanti-sse2-64.asm
-LOCAL_ASFLAGS_x86_64 += -D__x86_64__ -DPIC -DELF
+LOCAL_SRC_FILES_x86_64 += $(libjpeg_turbo_common_src_files_x86_64)
+LOCAL_ASFLAGS_x86_64 += -D__x86_64__ $(libjpeg_turbo_common_asflags)
 LOCAL_C_INCLUDES_x86_64 += $(LOCAL_PATH)/simd
 
-LOCAL_SRC_FILES_mips += jsimd_none.c
-LOCAL_SRC_FILES_mips64 += jsimd_none.c
+LOCAL_SRC_FILES_mips += $(libjpeg_turbo_common_src_files_mips)
+LOCAL_SRC_FILES_mips64 += $(libjpeg_turbo_common_src_files_mips)
 
-LOCAL_CFLAGS += -O3 -fstrict-aliasing
-LOCAL_CFLAGS += -Wno-unused-parameter
-LOCAL_CFLAGS += -DJDCT_DEFAULT=JDCT_IFAST
+LOCAL_CFLAGS += $(libjpeg_turbo_common_cflags)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 
 ifneq (,$(TARGET_BUILD_APPS))
@@ -92,7 +110,51 @@ ifneq (,$(TARGET_BUILD_APPS))
   LOCAL_SDK_VERSION := 17
 endif
 
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_WHOLE_STATIC_LIBRARIES = libjpeg_static
 LOCAL_MODULE := libjpeg
 include $(BUILD_SHARED_LIBRARY)
+
+
+# Build static library against the NDK
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := $(libjpeg_turbo_common_arm_mode)
+
+LOCAL_SRC_FILES := $(libjpeg_turbo_common_src_files)
+
+LOCAL_SRC_FILES_arm += $(libjpeg_turbo_common_src_files_arm)
+
+LOCAL_SRC_FILES_arm64 += $(libjpeg_turbo_common_src_files_arm64)
+
+LOCAL_SRC_FILES_x86 += $(libjpeg_turbo_common_src_files_x86)
+
+LOCAL_ASFLAGS_x86 += $(libjpeg_turbo_common_asflags)
+LOCAL_C_INCLUDES_x86 += $(LOCAL_PATH)/simd
+
+LOCAL_SRC_FILES_x86_64 += $(libjpeg_turbo_common_src_files_x86_64)
+LOCAL_ASFLAGS_x86_64 += -D__x86_64__ $(libjpeg_turbo_common_asflags)
+LOCAL_C_INCLUDES_x86_64 += $(LOCAL_PATH)/simd
+
+LOCAL_SRC_FILES_mips += $(libjpeg_turbo_common_src_files_mips)
+LOCAL_SRC_FILES_mips64 += $(libjpeg_turbo_common_src_files_mips)
+
+LOCAL_CFLAGS += $(libjpeg_turbo_common_cflags)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+LOCAL_SDK_VERSION := 17
+
+# Build as a static library.
+LOCAL_MODULE := libjpeg_static_ndk
+include $(BUILD_STATIC_LIBRARY)
+
+# Unset all created common variables
+libjpeg_turbo_common_arm_mode :=
+libjpeg_turbo_common_src_files :=
+libjpeg_turbo_common_src_files_arm :=
+libjpeg_turbo_common_src_files_arm64 :=
+libjpeg_turbo_common_src_files_x86 :=
+libjpeg_turbo_common_src_files_x86_64 :=
+libjpeg_turbo_common_asflags :=
+libjpeg_turbo_common_src_files_mips :=
+libjpeg_turbo_common_cflags :=
+
